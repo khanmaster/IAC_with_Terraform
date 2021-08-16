2e9dc8fddec27c33ef1f508afa6a82df8f5df7ff
Abdul Haseeb Shahrukh Khan <ahskhan@spartaglobal.com>
2021-08-16 16:01:56 +0100

    variable.tf added

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
 provider "aws" {
   region = "eu-west-1"
 }
+
+# launch an instance
+resource "aws_instance" "app_instance" {
+  ami           = "ami-039900c4ef89c6f9c"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  tags = {
+      Name = "eng89_shahrukh-terrafrom-app"
+  }
+   #The key_name to ssh into instance
+  key_name = var.aws_key_name
+  # aws_key_path = var.aws_key_path
+}
+
+# Let's create a VPC  
+resource "aws_vpc" "terraform_vpc_code_test" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "eng89_shahrukh_terraform_vpc"
+  }
+ }
+
+
+
---
+++ variable.tf
@@ -0,0 +1,11 @@
+
+
+variable "aws_key_name" {
+
+  default = "eng89_devops"
+}
+
+variable "aws_key_path" {
+
+  default = "~/.ssh/eng89_devops.pem"
+}
