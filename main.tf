e2eabb9c2b91bb147e000a795b1135046dad1923
Abdul Haseeb Shahrukh Khan <ahskhan@spartaglobal.com>
2021-08-17 10:04:06 +0100

    vpc and IGW RT added

--- main.tf
+++ main.tf
@@ -2,28 +2,44 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+# Let's create a VPC  
+resource "aws_vpc" "terraform_vpc_code_test" {
+  cidr_block       = var.cidr_block 
+  #"10.0.0.0/16"
+  instance_tenancy = "default"
+  
+  tags = {
+    Name = var.vpc_name
+  }
+} 
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "terraform_igw" {
+  vpc_id = aws_vpc.terraform_vpc_code_test.id
+  
+  tags = {
+    Name = var.igw_name
+  }
+}
+
+#Create Custom Route Table
+
+
 # launch an instance
 resource "aws_instance" "app_instance" {
-  ami           = "ami-039900c4ef89c6f9c"
+  ami           = var.app_ami_id
   instance_type = "t2.micro"
   associate_public_ip_address = true
   tags = {
-      Name = "eng89_shahrukh-terrafrom-app"
+      Name = var.name
   }
    #The key_name to ssh into instance
   key_name = var.aws_key_name
   # aws_key_path = var.aws_key_path
 }
+ 
 
-# Let's create a VPC  
-resource "aws_vpc" "terraform_vpc_code_test" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
 
-  tags = {
-    Name = "eng89_shahrukh_terraform_vpc"
-  }
- }
 
 
 
--- variable.tf
+++ variable.tf
@@ -1,4 +1,27 @@
 
+# Let's creatge a variable to apply DRY
+
+variable "name" {
+  default="Eng89_shahrukh_terraform_app"
+}
+
+variable "app_ami_id" {
+  default="ami-039900c4ef89c6f9c"
+}
+
+variable "vpc_id" {
+
+  default = "vpc-07e47e9d90d2076da"
+}
+variable "vpc_name" {
+  default = "Eng89_shahrukh_terraform_vpc"
+}
+variable "cidr_block" {
+  default="10.0.0.0/16"
+}
+variable "igw_name" {
+  default = "Eng89_shahrukh_terraform_igw"
+}
 
 variable "aws_key_name" {
 
